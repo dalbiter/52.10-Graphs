39f8486056eeba24e1fcc9a4fe2a5fea3acc126f
danx <rushn24@gmail.com>
2025-02-18 20:21:38 -0500

    added FriendGraph class

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 class PersonNode {
-    constructor(name, adjacent) {
+    constructor(name, adjacent = new Set()) {
         this.name = name;
-        this.adjacent = [];
+        this.adjacent = adjacent;
     }
 }
 
-const homer = new PersonNode('homer simpson');
-const marge = new PersonNode('marge simpson');
-homer.adjacent.push(marge);
-marge.adjacent.push(homer);
+// const homer = new PersonNode('homer simpson');
+// const marge = new PersonNode('marge simpson');
+// const maggie = new PersonNode('maggie simpson')
+// homer.adjacent.add(marge);
+// marge.adjacent.add(homer);
+// maggie.adjacent.add(homer);
+// maggie.adjacent.add(marge);
+// homer.adjacent.add(maggie);
+// marge.adjacent.add(maggie);
+
+class FriendGraph {
+    constructor() {
+        this.nodes = new Set();    
+    }
+    addPerson(node) {
+        this.nodes.add(node);
+    }
+    addPeople(peopleList) {
+        for(let node of peopleList) {
+            this.addPerson(node);
+        }
+    }
+    setFriends(person1, person2) {
+        person1.adjacent.add(person2);
+        person2.adjacent.add(person1);
+    }
+}
+
+
